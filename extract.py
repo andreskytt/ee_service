291d14dcb0f4d88cc0bdb8525c4a7c39ccc4b5d0
Andres Kütt <andres.kytt@gmail.com>
2015-02-27 17:08:00 +0100

    Added handling of non-CSVP types

--- extract.py
+++ extract.py
@@ -8,7 +8,7 @@ def convert(definition, content):
 	c = {'CPSV':cols[0].split('\t')[1].strip()}
 
 	firstDone = False
-	with codecs.open(content, encoding='LATIN4') as f:
+	with codecs.open(content, encoding='UTF-16') as f:
 		os = []
 		for line in f:
 			if not firstDone:
@@ -24,14 +24,22 @@ def convert(definition, content):
 			for d in data:
 				if d:
 					(tp, n) = cols[t].split('\t')
-					o[n.strip()] = {'@type':'CPSV:' + tp, '@value': d}
+					if tp:
+						o[n.strip()] = {'@type':'CPSV:' + tp, '@value': d}
+					else:
+						o[n.strip()] = d
 				t = t + 1
 
 			os.append(o)
 
 	return os
 
-output = convert('AgentDef.txt', 'agent.txt')
-output.extend(convert('PSDef.txt', 'ps.txt'))
+#output = convert('AgentDef.txt', 'agent.txt')
+#output.extend(convert('PSDef.txt', 'ps.txt'))
+
+#output = convert('MKMDef.txt', 'mkm_svc.txt')
+#output.extend(convert('AgentDef.txt','mkm_agent.txt'))
+output = convert('AgentDef.txt', 'mkm_agent.txt')
+output.extend(convert('MKMDef.txt', 'mkm_svc.txt'))
 
 print json.dumps(output, ensure_ascii=False,sort_keys=True, indent=4, separators=(',', ': ')).encode('utf-8')
